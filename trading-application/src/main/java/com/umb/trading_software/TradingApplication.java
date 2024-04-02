c652b85ebe06ca7f22f52a498fe122f5edae277c
Marek Mráz <mrazbb@gmail.com>
2024-04-02 19:04:27 +0300

    removed dependency issue

--- trading-application/src/main/java/com/umb/trading_software/TradingApplication.java
+++ trading-application/src/main/java/com/umb/trading_software/TradingApplication.java
@@ -1,21 +1,15 @@
 package com.umb.trading_software;
 
-import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 
 @SpringBootApplication
-@EnableMethodSecurity
 public class TradingApplication implements CommandLineRunner {
-	private static final Logger logger = LoggerFactory.getLogger(Demo3Application.class);
 
+	
 	public TradingApplication() {
 	}
 
@@ -25,6 +19,7 @@ public class TradingApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("Hello Spring Boot");
+		System.out.println("Hello World");
+
 	}
 }
