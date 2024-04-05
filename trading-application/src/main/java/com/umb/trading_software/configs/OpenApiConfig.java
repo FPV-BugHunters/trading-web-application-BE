3fe7d1d72ae032b65f66d37704e7d4692a53f5f6
Marek Mráz <mrazbb@gmail.com>
2024-04-05 16:17:56 +0300

    rename openapi titile

--- trading-application/src/main/java/com/umb/trading_software/configs/OpenApiConfig.java
+++ trading-application/src/main/java/com/umb/trading_software/configs/OpenApiConfig.java
@@ -10,6 +10,6 @@ import io.swagger.v3.oas.models.info.Info;
 public class OpenApiConfig {
 	@Bean
 	public OpenAPI carDatabaseOpenAPI() {
-		return new OpenAPI().info(new Info().title("Car REST API").description("My car stock").version("1.0"));
+		return new OpenAPI().info(new Info().title("Trading application").description("").version("1.0"));
 	}
 }
